9645570de61d0c0e7827d930078f8222164215a8
Mark Hanan <mhanan99@gmail.com>
2020-10-26 18:39:32 -0400

    Updated

---
+++ devicetypes/mhanan/virtual-switch3.src/virtual-switch3.groovy
@@ -0,0 +1,33 @@
+metadata {
+	definition (name: "Virtual Switch3", namespace: "mhanan", author: "SmartThings") {
+		capability "Actuator"
+		capability "Switch"
+ 		capability "Sensor"
+	}
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles {
+		standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
+			state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "off"
+		}
+		main "button"
+		details "button"
+	}
+}
+
+def parse(String description) {
+}
+
+
+def on() {
+	sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+	sendEvent(name: "switch", value: "off")
+}
