d5228f49f07165c42076dcab78ba7dbce5432d22
mhanan99 <mhanan99@hotmail.com>
2020-10-27 17:09:22 -0400

    Update unused-virtual-button.groovy

--- devicetypes/mhanan99/unused-virtual-button.src/unused-virtual-button.groovy
+++ devicetypes/mhanan99/unused-virtual-button.src/unused-virtual-button.groovy
@@ -12,12 +12,12 @@
  *
  *  Momentary Button Tile
  *
- *  Author: SmartThings
+ *  Author: SmartThings Edited by MH 10/26/2020
  *
- *  Date: 2013-05-01
+ *  Date: 2013-05-01  Revised by MH 10/26/2020
  */
 metadata {
-	definition (name: "UNUSED Virtual Button", namespace: "mhanan99", author: "Mark Hanan", mnmn: "SmartThings", vid: "generic-switch", ocfDeviceType: 'oic.d.switch') {
+	definition (name: "Virtual Button", namespace: "mhanan99", author: "Mark Hanan", mnmn: "SmartThings", vid: "generic-switch", ocfDeviceType: 'oic.d.switch') {
 		capability "Actuator"
 		capability "Switch"
 		capability "Momentary"
@@ -46,13 +46,17 @@ def parse(String description) {
 }
 
 def push() {
-	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+    sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
     sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
-	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+    sendEvent(name: "momentary", value: "pushed", isStateChange: true)
     sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
     sendEvent(name: "healthStatus", value: "online")
     sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
 }
 def on() {
+	push()
+}
+
+def off() {
 	push()
 }
