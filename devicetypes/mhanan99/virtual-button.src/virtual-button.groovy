33bad1aebfe468c978a3b869ea276df37aa2850d
Mark Hanan <mhanan99@gmail.com>
2020-10-27 17:33:59 -0400

    Updated Virtual Button (with new capabilities of Button)

---
+++ devicetypes/mhanan99/virtual-button.src/virtual-button.groovy
@@ -0,0 +1,62 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Momentary Button Tile
+ *
+ *  Author: SmartThings Edited by MH 10/26/2020
+ *
+ *  Date: 2013-05-01  Revised by MH 10/26/2020
+ */
+metadata {
+	definition (name: "Virtual Button", namespace: "mhanan99", author: "Mark Hanan", mnmn: "SmartThings", vid: "generic-switch", ocfDeviceType: 'oic.d.switch') {
+		capability "Actuator"
+		capability "Switch"
+		capability "Momentary"
+		capability "Sensor"
+        capability "Button"
+        }
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles(scale: 2){
+		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState("off", label: 'Push', action: "momentary.push", backgroundColor: "#ffffff", nextState: "on")
+				attributeState("on", label: 'Push', action: "momentary.push", backgroundColor: "#00a0dc")
+			}	
+		}
+		main "switch"
+		details "switch"
+	}
+}
+
+def parse(String description) {
+}
+
+def push() {
+    sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+    sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
+    sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+    sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+    sendEvent(name: "healthStatus", value: "online")
+    sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+}
+def on() {
+	push()
+}
+
+def off() {
+	push()
+}
